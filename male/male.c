30ab9c19bb6559fbdc4509dd03998738f6936676
wojtek <a@dupa.gov>
2022-03-19 00:32:59 +0100

    end of day, time foe tests

--- male/male.c
+++ male/male.c
@@ -24,6 +24,38 @@ queue_t *initialize_queue(int cap)
     return queue;
 }
 
+queue_t * resize_queue(queue_t *queue)
+{
+    queue_t *narr = realloc(queue->array, (queue->cap + 2) * sizeof(struct coord));
+    if (narr == NULL)
+    {
+        return NULL;
+    }
+    free(queue->array);
+    queue->cap += 2;
+    queue->array = narr;
+    if (queue->back < queue->front)
+    {
+        for (int i = 0; i < queue->back; i++)
+        {
+            queue->array[(queue->size -2 + i) % queue->size] = queue->array[i];
+        }
+        queue->back = (queue->size -2 + queue->back) % queue->size;
+    }
+    return queue;
+}
+
+void queue_destroy(queue_t *queue)
+{
+    for(int i = 0; i<length(queue->array)-1; i++)
+    {
+        free((&(queue->array)[i])->coord);
+        free(&(queue->array)[i]);
+    }
+    free(queue->array);
+    free(queue);
+}
+
 bool isEmpty(queue_t *queue)
 {
     return (queue->size == 0);
@@ -34,15 +66,24 @@ bool isFull(queue_t *queue)
     return (queue->size == queue->cap);
 }
 
-void add(queue_t *queue, coord_t item)
+queue_t* add(queue_t *queue, coord_t item)
 {
     if (isFull(queue))
     {
-        return;
+        queue_t* nqueue = resize_queue(queue);
+        if (nqueue == NULL)
+        {
+            return NULL;
+        }
+        else
+        {
+            queue = nqueue;
+        }
     }
     queue->back = (queue->back + 1) % queue->cap;
     queue->array[queue->back] = item;
     queue->size = queue->size + 1;
+    return queue;
 }
 
 coord_t *pop(queue_t *queue)
@@ -51,10 +92,8 @@ coord_t *pop(queue_t *queue)
     {
         return NULL;
     }
-    coord_t* res = &(queue->array[queue->front]);
-    queue->front = (queue->front + 1)% queue->cap;
-    queue->size = queue->size -1;
+    coord_t *res = &(queue->array[queue->front]);
+    queue->front = (queue->front + 1) % queue->cap;
+    queue->size = queue->size - 1;
     return res;
 }
-
-int main(){}
