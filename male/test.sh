888dabdf751a9d78c71da732f76d82f6d7910472
wojtek <a@dupa.gov>
2022-04-02 21:58:20 +0200

    koniec, alast

--- male/test.sh
+++ male/test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash 
 
 echo "starting tests"
+echo ""
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 
 for FILE in $2/*.in; do 
 
@@ -8,7 +13,7 @@ prefix="$2/"
 suffix=".in"
 name=${FILE#"$prefix"}
 name=${name%"$suffix"}
-echo "running test ${name}"
+echo -e "running test ${BLUE} ${name} ${NC}"
 
 OUT=$($1 < $FILE )
 ERROR=$($1 < $FILE 2>&1 1> /dev/null)
@@ -17,9 +22,9 @@ CORRERROR=$(<"${FILE::-2}err")
 
 if [ "$ERROR" == "$CORRERROR" -a "$CORROUT" == "$OUT" ]; 
 then
-    echo "test ${name} ok"
+    echo -e "${GREEN} test ${name} ok ${NC}"
 else
-    echo "test ${name} failed"
+    echo -e "${RED} test ${name} failed ${NC}"
 fi
 
 done;
