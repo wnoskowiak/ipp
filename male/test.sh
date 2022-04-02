011ff1f407465aae8be46001decb6de28f55e845
wojtek <a@dupa.gov>
2022-04-02 21:43:35 +0200

    kurwa proszę nie zjeb się teraz

--- male/test.sh
+++ male/test.sh
@@ -1,11 +1,27 @@
 #!/bin/bash 
 
+echo "starting tests"
+
 for FILE in $2/*.in; do 
 
-OUT=cat "${FILE::-2}out"
-ERROR=cat "${FILE::-2}err"
+prefix="$2/"
+suffix=".in"
+name=${FILE#"$prefix"}
+name=${name%"$suffix"}
+echo "running test ${name}"
 
-echo $OUT
-echo $ERROR
+OUT=$($1 < $FILE )
+ERROR=$($1 < $FILE 2>&1 1> /dev/null)
+CORROUT=$(<"${FILE::-2}out")
+CORRERROR=$(<"${FILE::-2}err")
+
+if [ "$ERROR" == "$CORRERROR" -a "$CORROUT" == "$OUT" ]; 
+then
+    echo "test ${name} ok"
+else
+    echo "test ${name} failed"
+fi
 
 done;
+
+echo "tests finished"
